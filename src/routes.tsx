cdeeb800b8a8d3974c35896253d836082cfe2423
thedarkmrx <ocamposource@gmail.com>
2026-09-17 18:37:23 -0500

    feat: add custom 404 page for routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,7 @@
 import { Home } from "@/pages/home/home"
+import { PageNotFound } from "@/pages/404/404page"
 
 export const getRoutes = (theme: string) => [
-  { path: "/", element: <Home theme={theme} /> },
+	{ path: "/", element: <Home theme={theme} /> },
+	{ path: "*", element: <PageNotFound /> },
 ]
