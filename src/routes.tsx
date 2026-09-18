5ad6c0e7c926d50773f47845d8585c6a24592b6f
thedarkmrx <ocamposource@gmail.com>
2026-09-17 19:37:30 -0500

    feat: theme is not used on Projects

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,6 @@ import { PageNotFound } from "@/pages/404/404page"
 
 export const getRoutes = (theme: string) => [
 	{ path: "/", element: <Home theme={theme} /> },
-	{ path: "/projects", element: <Projects theme={theme} /> },
+	{ path: "/projects", element: <Projects /> },
 	{ path: "*", element: <PageNotFound /> },
 ]
