34cdfb9128101d3d9e0485502373cb94d6916345
thedarkmrx <ocamposource@gmail.com>
2026-09-17 19:31:52 -0500

    feat: add projects path to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,9 @@
 import { Home } from "@/pages/home/home"
+import { Projects } from "@/pages/projects/projects"
 import { PageNotFound } from "@/pages/404/404page"
 
 export const getRoutes = (theme: string) => [
 	{ path: "/", element: <Home theme={theme} /> },
+	{ path: "/projects", element: <Projects theme={theme} /> },
 	{ path: "*", element: <PageNotFound /> },
 ]
