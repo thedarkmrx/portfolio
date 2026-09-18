3d94fb76f2948f48190c662d51f1d3f58fcc104f
thedarkmrx <ocamposource@gmail.com>
2026-09-17 19:35:04 -0500

    feat: comment theme because I don't use that in this moment

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -1,8 +1,9 @@
-interface ProjectsProps {
+/*interface ProjectsProps {
 	theme: string
 }
 
-export const Projects = ({ theme }: ProjectsProps) => {
+export const Projects = ({ theme }: ProjectsProps) => {*/
+export const Projets = () => {
 	return (
 		<main className="flex flex-col gap-14 pb-10 sm:gap-16 sm:pb-16">
 			<h1>Projects</h1>
