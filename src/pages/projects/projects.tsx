6543f492fb98806017f1d403f8884bc7bb63559e
thedarkmrx <ocamposource@gmail.com>
2026-09-17 19:36:05 -0500

    feat: change Projets to Projects

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -3,7 +3,7 @@
 }
 
 export const Projects = ({ theme }: ProjectsProps) => {*/
-export const Projets = () => {
+export const Projects = () => {
 	return (
 		<main className="flex flex-col gap-14 pb-10 sm:gap-16 sm:pb-16">
 			<h1>Projects</h1>
