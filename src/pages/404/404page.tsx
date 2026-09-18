d0f9a66967ca3d28e5f75acf0096e423d6e28386
thedarkmrx <ocamposource@gmail.com>
2026-09-17 19:03:08 -0500

    feat: custom 404 page not found

--- src/pages/404/404page.tsx
+++ src/pages/404/404page.tsx
@@ -1,8 +1,21 @@
+import {
+	Empty,
+	EmptyDescription,
+	EmptyHeader,
+	EmptyTitle,
+} from "@/components/ui/empty"
+
 export const PageNotFound = () => {
 	return (
-		<div>
-			<h2>404 Error</h2>
-			<p>Haha, you are dumb! The page you're looking for does not exist.</p>
-		</div>
+		<main className="flex flex-col items-center justify-center gap-14 pb-10 sm:gap-16 sm:pb-16">
+			<Empty>
+				<EmptyHeader>
+					<EmptyTitle>404 - Not Found</EmptyTitle>
+					<EmptyDescription>
+						The page you&apos;re looking for doesn&apos;t exist.
+					</EmptyDescription>
+				</EmptyHeader>
+			</Empty>
+		</main>
 	)
 }
