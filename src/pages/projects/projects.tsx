9a595cc915be7cb2aac122102758a6ce1fe8bbc3
thedarkmrx <ocamposource@gmail.com>
2026-09-17 19:32:24 -0500

    feat: add base info

---
+++ src/pages/projects/projects.tsx
@@ -0,0 +1,11 @@
+interface ProjectsProps {
+	theme: string
+}
+
+export const Projects = ({ theme }: ProjectsProps) => {
+	return (
+		<main className="flex flex-col gap-14 pb-10 sm:gap-16 sm:pb-16">
+			<h1>Projects</h1>
+		</main>
+	)
+}
