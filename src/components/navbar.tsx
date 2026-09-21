8ca0db7daf6984a2e32e0fa0dd5ca184d4a195db
thedarkmrx <ocamposource@gmail.com>
2026-09-21 17:35:47 -0500

    feat: remove some things to change theme and add ThemeButton to change theme

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,10 @@
-import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { NavLink } from "react-router"
-import { Sun, Moon } from "lucide-react"
-interface navbarProps {
-	theme: string
-	toggleTheme: () => void
-}
+import { ThemeButton } from "./change-theme"
+import { useTheme } from "./theme-provider"
 
-export const Navbar = ({ theme, toggleTheme }: navbarProps) => {
+export const Navbar = () => {
+	const { theme } = useTheme()
 	return (
 		<header className="mt-3 flex h-12 w-full flex-row items-center justify-between p-1 sm:mt-4">
 			<div className="flex flex-row gap-2">
@@ -44,20 +41,7 @@ export const Navbar = ({ theme, toggleTheme }: navbarProps) => {
 				</NavLink>
 			</div>
 			<div>
-				<Button
-					aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} theme`}
-					onClick={toggleTheme}
-					className={`size-8 rounded-full bg-transparent p-0 transition-colors duration-300 hover:cursor-pointer hover:bg-transparent ${theme === "dark"
-							? "text-gray-400 hover:text-white"
-							: "text-gray-500 hover:text-slate-950"
-						} `}
-				>
-					{theme === "dark" ? (
-						<Sun className="size-4 bg-transparent" />
-					) : (
-						<Moon className="size-4 bg-transparent" />
-					)}
-				</Button>
+				<ThemeButton />
 			</div>
 		</header>
 	)
