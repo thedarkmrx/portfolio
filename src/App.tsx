d08747a2de2f7095a52cb459301046a56fbf6164
thedarkmrx <ocamposource@gmail.com>
2026-09-21 17:35:26 -0500

    feat: change how to show navbar and remove navbar props

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,13 @@ import { getRoutes } from "./routes"
 import { BrowserRouter, Routes, Route } from "react-router"
 
 export function App() {
-	const { setTheme, theme } = useTheme()
-	const toggleTheme = () => {
-		const nextTheme =
-			theme === "dark" ? "light" : theme === "light" ? "dark" : "dark"
-		setTheme(nextTheme)
-	}
+	const { theme } = useTheme()
 	const routes = getRoutes(theme)
 	return (
 		<BrowserRouter>
 			<div className="flex min-h-svh justify-center px-4 sm:px-6">
 				<div className="flex w-full max-w-2xl flex-col gap-12 text-sm leading-loose sm:gap-16">
-					<Navbar theme={theme} toggleTheme={toggleTheme} />
+					<Navbar />
 					<Routes>
 						{routes.map((route) => (
 							<Route
