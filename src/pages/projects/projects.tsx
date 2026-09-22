2e197b57655c8a0a3a162061cd2d5c5b67bdf787
thedarkmrx <ocamposource@gmail.com>
2026-09-21 20:03:19 -0500

    feat: add section and minimal changes

--- src/pages/projects/projects.tsx
+++ src/pages/projects/projects.tsx
@@ -19,45 +19,47 @@ export const Projects = () => {
 	const { theme } = useTheme()
 
 	return (
-		<main className="pb-10 sm:pb-16">
+		<main className="gap-14 pb-10 sm:gap-16 sm:pb-16">
 			<Navbar />
-			<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8">
-				{projects.map((event, index) => {
-					const isLast = index === projects.length - 1
-					const shouldCenter = isOdd && isLast
+			<section className="flex flex-col gap-8 pt-16 sm:gap-12">
+				<div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-8">
+					{projects.map((project, index) => {
+						const isLast = index === projects.length - 1
+						const shouldCenter = isOdd && isLast
 
-					return (
-						<Card
-							key={event.id}
-							className={cn(
-								"relative w-full max-w-sm pt-0",
-								shouldCenter && "sm:col-span-2 sm:mx-auto"
-							)}
-						>
-							<div className="absolute inset-0 z-30 aspect-video bg-black/10" />
-							<img
-								src={theme === "dark" ? event.imageL : event.imageD}
-								alt={event.altImage}
-								className="relative z-20 aspect-video w-full object-cover"
-							/>
-							<CardHeader>
-								<CardAction>
-									<Badge variant="secondary">{event.status}</Badge>
-								</CardAction>
-								<CardTitle>{event.title}</CardTitle>
-								<CardDescription>{event.description}</CardDescription>
-							</CardHeader>
-							<CardFooter>
-								<Button className="w-full" asChild>
-									<a href={event.link} target="_blank">
-										View Project
-									</a>
-								</Button>
-							</CardFooter>
-						</Card>
-					)
-				})}
-			</div>
+						return (
+							<Card
+								key={project.id}
+								className={cn(
+									"relative w-full max-w-sm pt-0",
+									shouldCenter && "sm:col-span-2 sm:mx-auto"
+								)}
+							>
+								<div className="absolute inset-0 z-30 aspect-video bg-black/10" />
+								<img
+									src={theme === "dark" ? project.imageL : project.imageD}
+									alt={project.altImage}
+									className="relative z-20 aspect-video w-full object-cover"
+								/>
+								<CardHeader>
+									<CardAction>
+										<Badge variant="secondary">{project.status}</Badge>
+									</CardAction>
+									<CardTitle>{project.title}</CardTitle>
+									<CardDescription>{project.description}</CardDescription>
+								</CardHeader>
+								<CardFooter>
+									<Button className="w-full" asChild>
+										<a href={project.link} target="_blank">
+											View Project
+										</a>
+									</Button>
+								</CardFooter>
+							</Card>
+						)
+					})}
+				</div>
+			</section>
 		</main>
 	)
 }
