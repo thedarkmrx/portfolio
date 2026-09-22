d26907118d4dca5dec188732e44471a68177bd86
thedarkmrx <ocamposource@gmail.com>
2026-09-21 20:02:22 -0500

    feat: add another 3 projects

--- src/data/projects.data.ts
+++ src/data/projects.data.ts
@@ -26,4 +26,36 @@ export const projects: ProjectsData[] = [
 		],
 		status: "Deployed",
 	},
+	{
+		id: 2,
+		title: "Toki List",
+		altImage: "toki-list opengraph image",
+		description:
+			"A minimal todo list app to help you stay organized and productive.",
+		imageD: "https://tokilist.vercel.app/opengraph-image-d.png",
+		imageL: "https://tokilist.vercel.app/opengraph-image-l.png",
+		link: "https://tokilist.vercel.app/",
+		tech: [
+			{ name: "React", icon: " " },
+			{ name: "TailwindCSS", icon: " " },
+			{ name: "Vite", icon: " " },
+		],
+		status: "Deployed",
+	},
+	{
+		id: 3,
+		title: "Toki Notes",
+		altImage: "toki-notes opengraph image",
+		description:
+			"A minimal notes app to help you capture and organize your thoughts.",
+		imageD: "https://tokinotes.vercel.app/opengraph-image-d.png",
+		imageL: "https://tokinotes.vercel.app/opengraph-image-l.png",
+		link: "https://tokinotes.vercel.app/",
+		tech: [
+			{ name: "React", icon: " " },
+			{ name: "TailwindCSS", icon: " " },
+			{ name: "Vite", icon: " " },
+		],
+		status: "Deployed",
+	},
 ]
